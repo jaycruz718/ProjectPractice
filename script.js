f7112b1750f11bd098c19f3a7a1e75e09c534af9
Jennifer C.G. <jaycruz718@gmail.com>
2025-07-18 16:05:01 -0400

    Adding more details to the timer on script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const Days = document.getElementById('days');
+const Hours = document.getElementById('hours');
+const Minutes = document.getElementById('minutes');
+const Seconds = document.getElementById('seconds');
+
+const targetDate = newDate("October 31 2025 00:00:00").getTime();
+
+function 
